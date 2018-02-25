31c982e98103c95d137116365bee1bf5de580c0a
masrahi <36552322+masrahi@users.noreply.github.com>
2018-02-24 16:35:14 -0800

    Initial Solution Setup

---
+++ TR/TR.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TR.WebUI.Startup))]
+namespace TR.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
